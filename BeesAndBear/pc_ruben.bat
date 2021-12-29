a081384d47eadb06cc7fafd14997ca2befbd0622
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2021-12-29 19:19:40 +0100

    INIT

---
+++ BeesAndBear/pc_ruben.bat
@@ -0,0 +1,7 @@
+start /B go run C:\Users\ruben\Documents\Github\BeesAndBear\BeesAndBear\bear.go
+start /B go run C:\Users\ruben\Documents\Github\BeesAndBear\BeesAndBear\bear.go
+start /B go run C:\Users\ruben\Documents\Github\BeesAndBear\BeesAndBear\bear.go
+
+start /B go run C:\Users\ruben\Documents\Github\BeesAndBear\BeesAndBear\bees.go
+start /B go run C:\Users\ruben\Documents\Github\BeesAndBear\BeesAndBear\bees.go
+go run C:\Users\ruben\Documents\Github\BeesAndBear\BeesAndBear\bees.go
---
+++ go.mod
@@ -0,0 +1,5 @@
+module BeesAndBear
+
+go 1.17
+
+require github.com/streadway/amqp v1.0.0
