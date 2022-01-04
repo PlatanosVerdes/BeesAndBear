b1d7094f7093e9286965a5aec21aef06eb60367b
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-01-04 20:21:21 +0100

    Update generate_bat.py

--- generate_bat.py
+++ generate_bat.py
@@ -5,6 +5,7 @@ parser.add_argument("-p", "--producer", help="Producer name", default="Baloo", t
 parser.add_argument("-c", "--consumer", help="Consumer name file", default="BEENAMES", type=str)
 parser.add_argument("-n", "--number", help="Number of consumers", default=3, type=int)
 parser.add_argument("-o","--output", help="Output file", default="start_program.bat", type=str)
+parser.add_argument("-s", "--scriptFile", help="Go scripts for bear and bee", default="BeesAndBear", type=str)
 parser.add_argument("-r", "--run", help="Run the generated .bat", default=False, type=bool)
 args = parser.parse_args()
 
@@ -13,24 +14,24 @@ def main():
     out = rf'''@echo off
 
 :: BEAR
-start /B go run ''' + os.getcwd() + r'''\BeesAndBear\bear.go ''' + args.producer + r'''
+start /B go run ''' + os.getcwd() + r'''\\''' + args.scriptFile + r'''\bear.go ''' + args.producer + r'''
 
 :: BEES
 '''
     for i in range(args.number-1):
         random_name = random.choice(list(open("BEE_NAMES", encoding="utf8"))).split("\n")[0]
-        out += r'''start /B go run ''' + os.getcwd() + r'''\BeesAndBear\bees.go ''' + random_name + r'''
+        out += r'''start /B go run ''' + os.getcwd() + r'''\\''' + args.scriptFile + r'''\bees.go ''' + random_name + r'''
 '''
 
     random_name = random.choice(list(open("BEE_NAMES", encoding="utf8"))).split("\n")[0]
-    out += r'''go run ''' + os.getcwd() + r'''\BeesAndBear\bees.go ''' + random_name
+    out += r'''go run ''' + os.getcwd() + r'''\\''' + args.scriptFile + r'''\bees.go ''' + random_name
 
-    generator = open(os.getcwd() + f"\BeesAndBear\{args.output}", "w")
+    generator = open(os.getcwd() + f"\{args.scriptFile}\{args.output}", "w")
     generator.write(out)
     generator.close()
 
     if args.run:
-        os.system(f'cmd /k ".\BeesAndBear\{args.output}"')
+        os.system(f'cmd /k ".\{args.scriptFile}\{args.output}"')
 
 if __name__ == "__main__":
     main()
